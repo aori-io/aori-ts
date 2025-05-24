6e8e81e403f53d08ee60e504d67b2af7f79fbc80
Flooreyes <wyatt@aori.io>
2025-05-24 15:34:33 -0400

    v0.0.19

--- src/constants.ts
+++ src/constants.ts
@@ -9,28 +9,28 @@ export const CHAINS_MAP: Record<string, ChainInfo> = {
         chainKey: "ethereum",
         chainId: 1,
         eid: 30101,
-        address: "0x5F3CB376fb82402FcF6d917fD729542537b9C6ad",
+        address: "0x3DAe550CD4de51928a916C2918712005FdF2868a",
         blocktime: 12,
     },
     "base": {
         chainKey: "base",
         chainId: 8453,
         eid: 30184,
-        address: "0xF7c908EAA65FE48B201a5CD809Df9D28BdcB2C39",
+        address: "0xb76F2e06EA55dAD6387785e3838dfc6D48bD5546",
         blocktime: 2,
     },
     "arbitrum": {
         chainKey: "arbitrum",
         chainId: 42161,
         eid: 30110,
-        address: "0x0e9018EEEbA45d70A9087d5d05295843afa3160a",
+        address: "0xc6Ec0cddCbD6D46cd8526B6ac0E9b3ea69e71093",
         blocktime: 1,
     },
     "optimism": {
         chainKey: "optimism",
         chainId: 10,
         eid: 30111,
-        address: "0x725B3886ecf20Abd1D54227829DB125312DBe1E9",
+        address: "0xd49efE0FE627be5C016B55519C9733247deAA6c2",
         blocktime: 2,
     }
 };
