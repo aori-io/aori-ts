cc8f94624c86e0833213f72b63cd226d976cdbb4
Flooreyes <wyatt@aori.io>
2025-05-27 08:35:02 -0400

    chainInfo

--- src/constants.ts
+++ src/constants.ts
@@ -10,28 +10,24 @@ export const CHAINS_MAP: Record<string, ChainInfo> = {
         chainId: 1,
         eid: 30101,
         address: "0xeA5c82C81CCc0ba69616c6eae40A6EC7F7794d87",
-        blocktime: 12,
     },
     "base": {
         chainKey: "base",
         chainId: 8453,
         eid: 30184,
         address: "0xBF693fcE30E7B08965E10A7ECddc92818d6a2a1e",
-        blocktime: 2,
     },
     "arbitrum": {
         chainKey: "arbitrum",
         chainId: 42161,
         eid: 30110,
         address: "0x437266584AdEae66F0edF0B97d14F399C8463731",
-        blocktime: 1,
     },
     "optimism": {
         chainKey: "optimism",
         chainId: 10,
         eid: 30111,
         address: "0xAA8Ec1a2C2814aAc925107e2b3c94ee0E8367ab5",
-        blocktime: 2,
     }
 };
 
--- src/types.ts
+++ src/types.ts
@@ -79,7 +79,6 @@ export interface ChainInfo {
     chainId: number;
     eid: number;
     address: string;
-    blocktime: number;
 }
 
 //============================================
